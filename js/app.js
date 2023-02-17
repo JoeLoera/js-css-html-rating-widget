7f8dc1fe933baa7b274332718bd928cfafc0dbe5
JoeLoera <joeloera7@gmail.com>
2023-02-17 15:36:19 -0800

    Application is now complete

--- js/app.js
+++ js/app.js
@@ -4,7 +4,19 @@ const emotionElements = document.querySelectorAll(".rating-container div");
 console.log(emotionElements);
 
 function selectEmotion(e) {
+  mouth.classList.remove("happy-mouth", "indifferent-mouth", "sad-mouth");
   console.log("clicked", e.target.id);
+  const chosenEmotion = e.target.id;
+
+  if (chosenEmotion === "bad") {
+    mouth.classList.add("sad-mouth");
+  }
+  if (chosenEmotion === "okay") {
+    mouth.classList.add("indifferent-mouth");
+  }
+  if (chosenEmotion === "good") {
+    mouth.classList.add("happy-mouth");
+  }
 }
 
 emotionElements.forEach((emotionElement) =>
